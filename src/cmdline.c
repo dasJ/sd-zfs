87fd70c8b2fbb0b05fe561a9ab071b2871fc1555
Janne Heß <jannehess@gmail.com>
2016-11-01 14:41:16 +0100

    Write own generator for systemd

---
+++ src/cmdline.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cmdline.h"
+
+size_t getCmdline(char **cmdline) {
+	FILE *fd;
+	size_t linelen;
+
+	fd = fopen("/proc/cmdline", "r");
+	if (fd == NULL) {
+		printf("Can not open kernel command line\n");
+		return -1;
+	}
+	if (getline(cmdline, &linelen, fd) < 0) {
+		printf("Can not read kernel command line\n");
+		fclose(fd);
+		return -2;
+	}
+	fclose(fd);
+	return linelen;
+}
+
+int cmdline_getParam(char *paramName, char **value) {
+	char *cmdline = NULL;
+	char *param;
+
+	if (getCmdline(&cmdline) < 0) {
+		return -1;
+	}
+	// Parse tokens of command line
+	param = strtok(cmdline, " \t\n");
+	do {
+		if (strncmp(param, paramName, strlen(paramName)) == 0) {
+			*value = malloc((strlen(param) - strlen(paramName) + 1) * sizeof(char));
+			strcpy(*value, &(param[strlen(paramName)]));
+			free(cmdline);
+			return 0;
+		}
+	} while ((param = strtok(NULL, " \t\n")) != NULL);
+
+	free(cmdline);
+	return 1;
+}
